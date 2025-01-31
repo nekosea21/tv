a36d7a79dd3bd60b666c0eb60f99c2ccdf5f7258
nekosea21 <152998478+nekosea21@users.noreply.github.com>
2025-01-31 20:46:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Handle klik button GTV
+document.getElementById('gtv-button').addEventListener('click', function () {
+    window.open('https://sindikasi.inews.id/embed/video/YWdlbnQ9ZGVza3RvcCZ1cmw9aHR0cHMlM0ElMkYlMkZlbWJlZC5yY3RpcGx1cy5jb20lMkZsaXZlJTJGZ3R2JTJGaW5ld3NpZCZoZWlnaHQ9MTAwJTI1JndpZHRoPTEwMCUyNQ==', '_blank');
+  });
+  
+  // Handle klik button RCTI
+  document.getElementById('rcti-button').addEventListener('click', function () {
+    window.open('https://sindikasi.inews.id/embed/video/YWdlbnQ9ZGVza3RvcCZ1cmw9aHR0cHMlM0ElMkYlMkZlbWJlZC5yY3RpcGx1cy5jb20lMkZsaXZlJTJGcmN0aSUyRmluZXdzaWQmaGVpZ2h0PTEwMCUyNSZ3aWR0aD0xMDAlMjU=', '_blank');
+  });
+  
